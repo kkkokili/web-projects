6b3745c9a84193775d5eab78ee1d589cbc9dffa8
kokili <kokili.developer@outlook.com>
2025-10-02 01:50:11 -0700

    changes

--- fullstack/nodeJs/mongoose/fullstack-Express-Mongoose-todoList-app/app.js
+++ fullstack/nodeJs/mongoose/fullstack-Express-Mongoose-todoList-app/app.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import _ from 'lodash';
-const passWord = encodeURIComponent(process.env.passWord || '');
+const passWord = encodeURIComponent(process.env.passWord);
 
 const app = express();
 
