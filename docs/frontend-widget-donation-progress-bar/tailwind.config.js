bbcc3490562e3d1791119a49dc35c05349841df0
KO <80680320+kkkokili@users.noreply.github.com>
2025-11-22 23:52:04 -0800

    Update tailwind.config.js

--- docs/frontend-widget-donation-progress-bar/tailwind.config.js
+++ docs/frontend-widget-donation-progress-bar/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",   // ⭐ 一定要是相对当前目录的 ./src
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
